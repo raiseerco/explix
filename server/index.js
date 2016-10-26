a46fed5f157bdcb28e382ffc6f38376dd0888ee4
signumcode <signumcode@gmail.com>
2016-10-26 19:23:49 +0100

    refactoring

--- server/index.js
+++ server/index.js
@@ -3,22 +3,22 @@ import express from 'express';
 import path from 'path';
 import jayson from 'jayson';
 
-import ContractInstance from '../lib/contractinstance';
+import ContractInstance from '../lib/contractinstance'
 import { dummyConfig } from '../lib/helper'
 import Context from '../lib/context'
 import dummyContract from '../test/dummy.ratc'
+import bodyParser from 'body-parser'
 
-const app = express();
-const api = express.Router();
-const bodyParser = require('body-parser');
+const app = express()
+const api = express.Router()
 
 
-(async function() {
+;(async function() {
     /*Initialize context here*/
     async function dummyContext(configParams) {
-        const context = new Context(dummyConfig(configParams));
-        await context.initialize();
-        context.contractDefinitionManager.registerDefinition(dummyContract);
+        const context = new Context(dummyConfig(configParams))
+        await context.initialize()
+        context.contractDefinitionManager.registerDefinition(dummyContract)
         return context;
     }
     const context = await dummyContext();
@@ -29,77 +29,74 @@ const bodyParser = require('body-parser');
     )
 
 
+    /*request example
+              {   
+                  "jsonrpc": "2.0", 
+                  "id":"1",
+                  "method":"getContractIds"
+              }
+          */
+    async function getContractIds(args, callback) {
+        const contracts = await context.contractInstanceManager.getContractInstances()
+        const contractIds = contracts.map(el => el.getChainID())
+        callback(null, contractIds)
+    }
 
-    //requests path POST '/contracts'
-    const contracts = {
-        /*request example
-            {   
-                "jsonrpc": "2.0", 
-                "id":"1",
-                "method":"getContractIds"
-            }
-        */
-        getContractIds: async function(args, callback) {
-            const contracts = await context.contractInstanceManager.getContractInstances();
-            const contractIds = contracts.map(el => el.getChainID());
-            callback(null, contractIds);
-        },
+    /*request example
+       {"jsonrpc": "2.0", 
+        "id":"1",
+        "method":"getFields", 
+        "params": {"id": {{contract id}}} }
+    */
+    async function getFields(args, callback) {
+        const contract = await context.contractInstanceManager.getInstanceByChainID(args.id)
 
-        /*request example
-           {"jsonrpc": "2.0", 
-            "id":"1",
-            "method":"getFields", 
-            "params": {"id": {{contract id}}} }
-        */
-        getFields: async function(args, callback) {
-            const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
+        if (!contract) {
+            var error = { code: 404, message: 'Cannot find chain id ' + args.id }
+            callback(error) // will return the error object as given
+        } else {
+            return callback(null, contract.getFields())
+        }
+    }
 
-            if (!contract) {
-                var error = { code: 404, message: 'Cannot find chain id ' + args.id };
-                callback(error); // will return the error object as given
-            } else {
-                return callback(null, contract.getFields());
-            }
-        },
+    /*request example
+    {   "jsonrpc": "2.0", 
+        "id":"1",
+        "method":"getActions", 
+        "params": {"id": {{contract id}}} }
+    */
+    async function getActions(args, callback) {
+        const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
 
-        /*request example
-        {   "jsonrpc": "2.0", 
-            "id":"1",
-            "method":"getActions", 
-            "params": {"id": {{contract id}}} }
-        */
-        getActions: async function(args, callback) {
-            const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
+        if (!contract) {
+            var error = { code: 404, message: 'Cannot find chain id ' + args.id }
+            callback(error) // will return the error object as given
+        } else {
+            return callback(null, contract.getApplicableActions())
+        }
 
-            if (!contract) {
-                var error = { code: 404, message: 'Cannot find chain id ' + args.id };
-                callback(error); // will return the error object as given
-            } else {
-                return callback(null, contract.getApplicableActions());
-            }
+    }
 
-        },
+    /*request example
+    {   "jsonrpc": "2.0", 
+        "id":"1",
+        "method":"getActionParams", 
+        "params": {"id": {{contract id}}} }
+    */
+    async function getActionParams(args, callback) {
+        const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
 
-        /*request example
-        {   "jsonrpc": "2.0", 
-            "id":"1",
-            "method":"getActionParams", 
-            "params": {"id": {{contract id}}} }
-        */
-        getActionParams: async function(args, callback) {
-            const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
-
-            if (!contract) {
-                var error = { code: 404, message: 'Cannot find chain id ' + args.id };
-                callback(error); // will return the error object as given
-            } else {
-                const result = contract.getActionParams()
-                return callback(null, result);
-            }
-        },
+        if (!contract) {
+            var error = { code: 404, message: 'Cannot find chain id ' + args.id }
+            callback(error) // will return the error object as given
+        } else {
+            const result = contract.getActionParams()
+            return callback(null, result);
+        }
+    }
 
 
-         /*request example
+    /*request example
         {   "jsonrpc": "2.0", 
             "id":"1",
             "method":"getActionParams", 
@@ -110,17 +107,16 @@ const bodyParser = require('body-parser');
                 } 
             }
         */
-        performAction: async function(args, callback) {
-            const contract = await context.contractInstanceManager.getInstanceByChainID(args.id);
-            const name = args.name;
-            try{
-                const object = JSON.parse()
-            }catch(err){
-            }
-            if( !name || !name){
-                var error = { 
-                    code: 400, 
-                    message: `Bad request. Provide a valid action name with action object example  \n   
+    async function performAction(args, callback) {
+        const contract = await context.contractInstanceManager.getInstanceByChainID(args.id)
+        const name = args.name
+        try {
+            const object = JSON.parse()
+        } catch (err) {}
+        if (!name || !object) {
+            var error = {
+                code: 400,
+                message: `Bad request. Provide a valid action name with action object example  \n   
                     { 
                         "jsonrpc": "2.0", 
                         "id":"1",
@@ -131,37 +127,45 @@ const bodyParser = require('body-parser');
                             "object" : {{action object}}
                         } 
                     }`
-                };
-                return callback(error); // will return the error object as given
-            }
-            if (!contract) {
-                var error = { code: 404, message: 'Cannot find chain id ' + args.id };
-                return callback(error); // will return the error object as given
-            } else {
-                return callback(null, contract.performAction(req.param.chainId));
-            }
+            };
+            return callback(error); // will return the error object as given
         }
+
+        if (!contract) {
+            var error = { code: 404, message: 'Cannot find chain id ' + args.id }
+            return callback(error); // will return the error object as given
+        }
+        return callback(null, contract.performAction(name, object))
     }
-    //register post contract rout for json rpc protocol 
-    app.post('/contracts',
-        bodyParser.json(),
-        bodyParser.urlencoded({
-            extended: true
-        }),
-        jayson.server(contracts).middleware() 
-    )
+
+    //requests path POST '/contracts'
+    const contracts =
+        //register post contract rout for json rpc protocol 
+        app.post('/contracts',
+            bodyParser.json(),
+            bodyParser.urlencoded({
+                extended: true
+            }),
+            jayson.server({
+                getContractIds,
+                getFields,
+                getActions,
+                getActionParams,
+                performAction
+            }).middleware()
+        )
 
     app.use(function(err, req, res, next) {
-        if (!err || err.status == 404) return next(); // you also need this line
-        console.error( err.stack );
-        console.log(err);
-        res.status(500).send(err);
-    });
+        if (!err || err.status == 404) return next() // you also need this line
+        console.error(err.stack)
+        console.log(err)
+        res.status(500).send(err)
+    })
 
 
     const server = app.listen(4000, function() {
-        console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV);
-        console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
-        console.log('environment mode ' + app.get('env'));
-    });
+        console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV)
+        console.log('Example app listening at http://%s:%s', server.address().address, server.address().port)
+        console.log('environment mode ' + app.get('env'))
+    })
 }())
