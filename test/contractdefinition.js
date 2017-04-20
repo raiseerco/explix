77bdc6d37eec52ef86cc80ae9d67dbfbb388e032
Alex Mizrahi <alex.mizrahi@gmail.com>
2017-04-20 16:29:38 +0700

    download definition from URL, contract definition name, package update

--- test/contractdefinition.js
+++ test/contractdefinition.js
@@ -1,3 +1,16 @@
-/**
- * Created by macbook on 4/15/17.
- */
+import { expect } from 'chai'
+import Context from '../lib/context'
+import { dummyConfig } from '../lib/helper'
+
+
+
+describe("ContractDefinition", function() {
+    it("load from url", async function () {
+        let context = new Context(dummyConfig());
+        await context.initialize()
+        const cdm = context.contractDefinitionManager;
+        await cdm.registerDefinitionFromURL("http://localhost:5000/futuristic.r2o");
+
+    });
+})
+
