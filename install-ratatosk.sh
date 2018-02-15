41412ddcb3c4e6df4f3dc9bf9ef4855f5cb7e320
Alex Mizrahi <alex.mizrahi@gmail.com>
2018-02-16 00:02:23 +0200

    use open ratatosk URL

--- install-ratatosk.sh
+++ install-ratatosk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-git clone git@bitbucket.org:chromawallet/ratatosk.git
+git clone https://killerstorm@bitbucket.org/chromawallet/ratatosk.git
 cd ratatosk
 npm install
 npm run compile
