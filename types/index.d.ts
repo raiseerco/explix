0af9bd8b2e65504d9b56873abf08bf1df3778063
Alex Mizrahi <alex.mizrahi@gmail.com>
2018-04-03 14:40:06 +0300

    v4.0.6, added type declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,26 +2,75 @@
  * Copyright (c) 2016-2018 ChromaWay AB. Licensed under the Apache License v. 2.0, see LICENSE
  */
 
-declare module 'esplix' {
-    import {ParameterInfo} from "ratatosk/dist/contractdefinition";
-    import {ProcessedMessage} from "ratatosk/dist/contractinstance";
+import {ParameterInfo} from "ratatosk/dist/contractdefinition";
+import {ProcessedMessage} from "ratatosk/dist/contractinstance";
 
-    class EsplixContext {
-        constructor(params: any);
-        initialize(): Promise<void>;
-        update(): Promise<void>;
-    }
+export interface PrincipalIdentity {
+    isSetUp(): boolean;
 
-    interface ParameterInfos {
-        [name:string]: ParameterInfo;
-    }
+    getID(): string;
 
-    interface Parameters {
-        [name: string]: any;
-    }
+    importIdentity (encodedKeyPair: any): Promise<void>;
 
-    interface ContractInstance {
-        performAction (action: string, params: Parameters): Promise<ProcessedMessage | undefined>
-    }
+    generateIdentity (entropy?: any): Promise<void>;
+
+    getRawKeyPair(): any;
+
+    getKeyPair(): any;
+
+    getPublicKey (): any;
+
+    getPrivateKey (): any;
+}
+
+export interface ContractDefinition {
+
+}
+
+export interface ContractDefinitionManager {
+    registerDefinition(defdata: any): any;
+
+    registerDefinitionFromURL(url: string): Promise<ContractDefinition>;
+}
+
+export interface ContractInstanceManager {
+    sync(): Promise<boolean>
+
+    getContractInstances(): ContractInstance[];
+
+    getInstanceByChainID(id: string): ContractInstance | undefined;
+
+    joinContractInstance (chainID: string, metadata: any): Promise<ContractInstance>;
+
+    createContractInstance (contractDefinition: ContractDefinition, parameters: Parameters): Promise<ContractInstance>;
+}
+
+export class EsplixContext {
+    principalIdentity: PrincipalIdentity;
+    contractDefinitionManager: ContractDefinitionManager;
+    contractInstanceManager: ContractInstanceManager;
+
+    constructor(params: any);
+
+    initialize(): Promise<void>;
+
+    update(): Promise<void>;
+}
+
+
+export function dummyConfig(a?: any): any;
+
+export function postchainConfig(a: any): any;
+
+export interface ParameterInfos {
+    [name: string]: ParameterInfo;
+}
+
+export interface Parameters {
+    [name: string]: any;
+}
+
+export interface ContractInstance {
+    performAction (action: string, params: Parameters): Promise<ProcessedMessage | undefined>
 }
 
